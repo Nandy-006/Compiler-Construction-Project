e9d36e9b4ecbe1b679bedb4fa1683515e1e7523a
Amogh-Bharadwaj <amoghrb01@gmail.com>
2022-02-13 17:06:54 +0530

    Class for identifier

--- Identifier.py
+++ Identifier.py
@@ -2,26 +2,36 @@
 
 class Identifier:
     def __init__(self,test_string:str):
-        self.firstIsAlphabet = False
         self.testString = test_string
     
     def isAlphabet(self):
-
+      # Checks every character in string is alphabet or underscore
       for test_character in self.testString:
-          if not (isalpha(test_character) or test_character=="_"):
-              return False
+        if not (test_character.isalpha() or test_character=="_"):
+            return False
 
       return True
         
-
     def isNumber(self):
-        pass
+        return self.testString.isnumeric()
 
     def isAlphaNumeral(self):
-        pass
-
+        return self.testString.isalnum()
+        
     def isIdentifier(self):
-        pass
+        if not self.testString[0].isalpha():
+            return False
+        
+        return self.isAlphaNumeral()
+    
+        
+
+
+
+        
+
+
+        
 
 
 
